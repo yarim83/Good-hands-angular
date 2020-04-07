eff31e8c96fd5a9766edd2f58378cbadd079831f
yarim83 <GeminI83!>
2020-04-07 20:24:51 +0200

    Adding Donation integration, adding institution integration

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DonationService} from '../services/donation.service';
 import {Observable} from 'rxjs';
+import {InstitutionService} from '../services/institution.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -11,8 +12,11 @@ export class LandingPageComponent implements OnInit {
 
   private donationCount: number;
   private bagsDonated: number;
+  private institutionListLineOne: Array<Institution>;
+  private institutionListLineTwo: Array<Institution>;
 
-  constructor(private donationService: DonationService) {
+  constructor(private donationService: DonationService,
+              private institutionService: InstitutionService) {
   }
 
   ngOnInit() {
@@ -21,10 +25,14 @@ export class LandingPageComponent implements OnInit {
 
   reloadData() {
     this.donationService.getDonationCount().subscribe(data => {
-      console.log(data);
       this.donationCount = data['donationBags'];
       this.bagsDonated = data['bagsDonated'];
     });
+    this.institutionService.getInstitutionLimitList(0,2).subscribe( data =>{
+      this.institutionListLineOne = data['content'];
+    });
+    this.institutionService.getInstitutionLimitList(1,2).subscribe( data =>{
+      this.institutionListLineTwo = data['content'];
+    });
   }
-
 }
--- src/app/services/institution.service.ts
+++ src/app/services/institution.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstitutionService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/institution/';
+
+  constructor(private httpClient: HttpClient) {
+    this.getInstitutionList();
+  }
+
+  getInstitutionList(): Observable<Array<Institution>> {
+    return this.httpClient.get<Array<Institution>>(`${this.baseUrl}`);
+  }
+
+  getInstitutionLimitList(page: number, limit: number): Observable<Array<Institution>> {
+    return this.httpClient.get<Array<Institution>>(`${this.baseUrl}` + 'page/' + page + '/limit/' + limit);
+  }
 }
